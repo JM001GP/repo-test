2daf76df1d0e06f95e65b24e497ec1ac21990c78
GARCJ969 <Juan.M.Garcia.-ND.disney.com>
2023-06-24 22:33:25 -0500

    divergencia M rebase

--- fileB.py
+++ fileB.py
@@ -1,5 +1,10 @@
 b=1
+<<<<<<< HEAD
 c='Otra divergencia preparaos porque lo voy a resolver'
 d='Nueva y diferente divergencia'
+=======
+c='una divergencia en el mundo'
+d='un mundo divergente'
+>>>>>>> c27c253 (divergencia M)
 c=2
 d='Otra divergencia'
