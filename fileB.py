9ba55cffe9cca077c582c9d458d936515b711328
GARCJ969 <Juan.M.Garcia.-ND.disney.com>
2023-06-24 23:06:43 -0500

    change

--- fileB.py
+++ fileB.py
@@ -1,10 +1,5 @@
 b=1
-<<<<<<< HEAD
-c='Otra divergencia preparaos porque lo voy a resolver'
-d='Nueva y diferente divergencia'
-=======
 c='una divergencia en el mundo'
 d='un mundo divergente'
->>>>>>> c27c253 (divergencia M)
 c=2
 d='Otra divergencia en el mundo'
--- fileC.py
+++ fileC.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-c='Hola mundo cruel, esta es una divergencia'
-=======
 c='Hola mundo, esta es una divergencia y la vas a resolver'
->>>>>>> 4bdb5ba (divergencia X)
